5f8f055667ca50c5ea55870cdc993aa31e018af2
Normalno100 <msomsin@mail.ru>
2026-05-10 11:24:38 +0300

    start

---
+++ src/main/java/com/example/PowerAssetIntelligence/PowerAssetIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.PowerAssetIntelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerAssetIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerAssetIntelligenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/PowerAssetIntelligence/PowerAssetIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PowerAssetIntelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerAssetIntelligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
